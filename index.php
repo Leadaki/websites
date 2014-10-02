ce15ded9a9a51975edfa4937b707c09bb4dd0423
Jesus Urrutia <jesus.urrutia@gmail.com>
2014-10-02 17:36:57 -0300

    Use new application class

--- index.php
+++ index.php
@@ -6,6 +6,9 @@ require __DIR__ . '/vendor/autoload.php';
 // -- Load used class
 use Aura\Router\RouterFactory;
 use Leadaki\Frontend\Service\LoadSiteDataService;
+use Leadaki\Frontend\Application\Application;
+use Leadaki\Frontend\Service\AltoRouterService;
+use Leadaki\Frontend\Service\TwigTemplateService;
 
 // -- Load config file or redirect to install script
 if (file_exists(__DIR__ . '/config/config.php')) {
@@ -24,6 +27,37 @@ $loadSiteDataService = new LoadSiteDataService(
 );
 
 $site = $loadSiteDataService->getSite();
+
+$routerService = new AltoRouterService(array(
+    'base_path' => parse_url($config['app']['base_url'], PHP_URL_PATH)
+));
+
+$templateService = new TwigTemplateService(array(
+    'templates_folder' => $config['folders']['templates'],
+    'templates_cache_folder' => $config['folders']['templates_cache'],
+    'debug' => $config['app']['debug'],
+));
+
+$templateService->addGlobal('config', $config);
+$templateService->addGlobal('site', $site);
+
+$templateService->addFunction('url_generate', function($name, $parameters = array()) use ($routerService) {
+    return $routerService->generate($name, $parameters);
+});
+
+if ($config['app']['debug']) {
+    $templateService->addExtension(new Twig_Extension_Debug());
+}
+
+$app = new Application(
+    $routerService,
+    $templateService,
+    $config,
+    $site
+);
+
+$app->run();
+exit;
 
 $routerCachePath = sprintf('%s/%s', $config['folders']['cache'], 'router.cache');
 $routerFactory = new RouterFactory();
